5a01105ae146edb37bcf1187c9d964635882ab2a
francisco august <fraciscoaugust19921992@gmail.com>
2022-08-26 22:00:12 -0500

    agregando en proyecto pagina de imobiliaria San Jose y Gallery HARRY

--- js/js.js
+++ js/js.js
@@ -41,13 +41,26 @@ const imagen1=document.getElementById("img1")
  });
  obersvador.observe(imagen1);
 
-
-
 //script proyectos click imagen//
  document.addEventListener("click",function (e){
+  
+ var texto_proyecto1=document.querySelector(".inmobiliaria-san-jose");
+ var texto_proyecto2=document.querySelector(".harrypotter");
+ console.log(texto_proyecto2.textContent)
+
+ console.log(texto_proyecto1)
+ var texto_modal= document.querySelector(".texto-modal")
+
     console.log(e)
   if(e.target.classList.contains("gallery-item")){
+
         const src = e.target.getAttribute("src");
+     if (src==="img/proyecto1.png") {
+        texto_modal.innerHTML=texto_proyecto1.textContent
+     }
+     else if (src=="img/proyecto3.png") {
+      texto_modal.innerHTML=texto_proyecto2.textContent
+     }
         document.querySelector(".modal-img").src = src;
       document.querySelector(".capa-a");
         const myModal = new bootstrap.Modal(document.getElementById('gallery-popup'));
